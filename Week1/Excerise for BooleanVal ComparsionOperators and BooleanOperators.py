27a500e6f6ee08f53d97305ee27b0718a4411c1f
Krish <radkrish.v@gmail.com>
2020-10-20 13:12:00 -0400

    Add files via upload

---
+++ Week1/Excerise for BooleanVal ComparsionOperators and BooleanOperators.py
@@ -0,0 +1,20 @@
+#Boolean Values (True, False)
+# Comparsion Operators ( ==, !=, <, >, <=, >=
+# Boolean Operators AND , OR, NOT
+
+print ('input number 1')
+var1= input()
+print ('input number 2')
+var2= input()
+result=int(var1)==int(var2)
+print('are the numbers same?')
+print (result)
+
+print ('Enter you catname')
+mycatname=input()
+print ('are numbers 1 and 2 are same and your cat name is pete?')
+result=int(var1)==int(var2) and mycatname=='pete'
+print(result)
+
+
+
